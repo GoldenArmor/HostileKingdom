52623aed062946a4165258a3a60f72afa15d9c90
Quooler <marc.vallverdu.98@gmail.com>
2017-11-08 23:20:19 +0100

    same

---
+++ Assets/Scripts/CardsBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardsBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject[] playableUnits; 
+
+	void Start ()
+    {
+        playableUnits = GameObject.FindGameObjectsWithTag("PlayableUnit");
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
