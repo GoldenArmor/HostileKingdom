ce36b1424c885d3cd00daf0027edb15849d53505
Quooler <marc.vallverdu.98@gmail.com>
2017-12-01 00:24:40 +0100

    Added hero class

---
+++ Assets/GameCharacters/PlayableUnits/Scripts/Hero.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    float selectTarget;
+    float currentSelectTarget;
+
+    float skillRadius; 
+	
+	void AttackUpdate(Vector3 center)
+    {
+        
+    }
+
+    void SelectTarget()
+    {
+        currentSelectTarget -= Time.deltaTime;
+        if(currentSelectTarget < 0)
+        {
+            AttackUpdate(); 
+        }
+    }
+}
