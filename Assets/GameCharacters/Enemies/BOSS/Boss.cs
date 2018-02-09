e25ce17f500acd7832d12f3abf85eddf8a89e588
Quooler <marc.vallverdu.98@gmail.com>
2018-02-09 12:06:47 +0100

    Added the boss and the boss pahses scripts

---
+++ Assets/GameCharacters/Enemies/BOSS/Boss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    [Header("Stats")]
+    [SerializeField]
+    float startingLife;
+    float currentLife;
+
+    [Header("Boss Phases")]
+    BossPhase currentBossPhase;
+
+    void Start()
+    {
+        currentBossPhase.InternalStart();
+    }
+
+    void Update()
+    {
+        UpdatePhase(currentBossPhase);
+    }
+
+    void UpdatePhase(BossPhase bossPhase)
+    {
+        bossPhase.InternalUpdate();
+    }
+
+    public void GoNext(BossPhase nextBossPhase)
+    {
+        if(nextBossPhase != null)
+        {
+            currentBossPhase = nextBossPhase;
+        }
+    }
+}
---
+++ Assets/GameCharacters/Enemies/BOSS/BossPhase.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPhase : MonoBehaviour
+{
+    public virtual void InternalStart()
+    {
+    }
+
+    public virtual void InternalUpdate()
+    {
+    }
+
+    protected virtual void Firstbehaviour()
+    {
+        // 1/4 ataque en area. 
+        // 2/4 escudo
+        // 3/4 spawn esqueletos
+        // 4/4 muerte
+    }
+
+    protected virtual void SecondBehaviour()
+    {
+        // 1/4 barrido de fuego
+        // 2/4 volar 
+        // 3/4 barrido de fuego
+    }
+
+    protected virtual void ThirdBehaviour()
+    {
+        // 2/4 ataque de garras
+        // 3/4 caen bolas del aire
+    }
+}
