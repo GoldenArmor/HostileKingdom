2cb1f9ca65f4141e2d0fffc3a05dfbfc48a34f02
Quooler <marc.vallverdu.98@gmail.com>
2018-04-13 01:42:40 +0200

    Added textures and pause screen

--- Assets/Player/Scripts/StatsManager.cs
+++ Assets/Player/Scripts/StatsManager.cs
@@ -5,12 +5,26 @@ using UnityEngine.UI;
 
 public class StatsManager : MonoBehaviour
 {
+    [SerializeField]
     Text lives;
+    [SerializeField]
     Text money;
+    [SerializeField]
+    GameObject pausePanel; 
 
     void Update()
     {
         lives.text = Player.lives.ToString();
         money.text = Player.money.ToString();
+    }
+
+    public void Paused()
+    {
+        pausePanel.SetActive(true); 
+    }
+
+    public void UnPaused()
+    {
+        pausePanel.SetActive(false);
     }
 }
