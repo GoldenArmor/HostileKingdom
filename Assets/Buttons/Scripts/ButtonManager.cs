06b14e06277ba56012f3cccfbf913a90690abc23
Quooler <marc.vallverdu.98@gmail.com>
2018-04-13 14:22:12 +0200

    Added music and sounds

--- Assets/Buttons/Scripts/ButtonManager.cs
+++ Assets/Buttons/Scripts/ButtonManager.cs
@@ -15,6 +15,11 @@ public class ButtonManager : MonoBehaviour
     const int lostScene = 5;
     const int wonScene = 6;
 
+    [SerializeField]
+    AudioSource audioSource;
+    [SerializeField]
+    AudioSource musicAudioSource;
+
     void Start()
     {
         levelLogic = GameObject.FindGameObjectWithTag("Manager").GetComponent<LevelLogic>();
@@ -22,7 +27,8 @@ public class ButtonManager : MonoBehaviour
 
     public void ClickRandomSound()
     {
-        //audioManager.Play(Random.Range(0, audioManager.sounds.Length));
+        //audioPlayer.Play(audioPlayer.sfxClips[Random.Range(0, audioPlayer.sfxClips.Length)], 1, 1, false, false, "Sounds");
+        audioSource.PlayOneShot(audioSource.clip); 
     }
 
     public void QuitGame()
@@ -33,41 +39,49 @@ public class ButtonManager : MonoBehaviour
     #region SceneLoad
     public void ChangetoNextScene()
     {
+        musicAudioSource.Stop(); 
         levelLogic.StartLoad(levelLogic.nextScene);
     }
 
     public void ChangetoBackScene()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(levelLogic.backScene);
     }
 
     public void ChangeToMenu()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(titleScene);
     }
 
     public void ChangeToGameplay()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(gameplayScene);
     }
 
     public void ChangeToOptions()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(optionsScene);
     }
 
     public void ChangeToTavern()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(tavernScene);
     }
 
     public void ChangeToLost()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(lostScene);
     }
 
     public void ChangeToWin()
     {
+        musicAudioSource.Stop();
         levelLogic.StartLoad(wonScene);
     }
     #endregion
