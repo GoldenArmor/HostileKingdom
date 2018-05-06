08d5afa3bfb85af6c382610b7f61ab0103051eb1
Quooler <marc.vallverdu.98@gmail.com>
2018-05-07 00:34:58 +0200

    Path mesh done, enemies go to castle

--- Assets/WavesManager/Scripts/EnemyWaveManager.cs
+++ Assets/WavesManager/Scripts/EnemyWaveManager.cs
@@ -1,9 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class EnemyWaveManager : MonoBehaviour
 {
-    [SerializeField]
     public static int enemiesAlive = 0;
+    public static int wavesCount = 0;
+    [SerializeField]
+    Text waveCounterText;
+
+    void Start()
+    {
+        waveCounterText.text = ("0/10");    
+    }
+
+    public void ChangeWavesCount(int newValue)
+    {
+        waveCounterText.text = (newValue + "/10");
+    }
 }
