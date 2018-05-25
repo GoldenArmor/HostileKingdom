b86073545d69ddb7919b6612d07f9971278bec53
Quooler <marc.vallverdu.98@gmail.com>
2018-05-25 12:10:32 +0200

    Golem parts separated anda attached to the animated body

---
+++ Assets/GameCharacters/Enemies/Golem/Scripts/Golem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] bodyParts;
+    Rigidbody[] rigidbodies; 
+    bool isDead; 
+
+	void Start ()
+    {
+        rigidbodies = new Rigidbody[bodyParts.Length]; 
+        for(int i = 0; i < bodyParts.Length; i++)
+        {
+            rigidbodies[i] = bodyParts[i].GetComponent<Rigidbody>(); 
+        }
+	}
+	
+	void FixedUpdate ()
+    {
+		
+	}
+
+    public void Die()
+    {
+        isDead = true; 
+    }
+}
