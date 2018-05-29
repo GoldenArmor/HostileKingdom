48293d24d9b5157667044f861c470764c7db8bcf
Quooler <marc.vallverdu.98@gmail.com>
2018-05-29 18:56:33 +0200

    Testing body explosion

--- Assets/GameCharacters/Enemies/Scripts/BodyExplosion.cs
+++ Assets/GameCharacters/Enemies/Scripts/BodyExplosion.cs
@@ -8,6 +8,11 @@ public class BodyExplosion : MonoBehaviour
     [SerializeField]
     GameObject[] bodyParts;
     Rigidbody[] rigidbodies;
+    BoxCollider[] bodyPartColliders;
+    Vector3[] iniPositions;
+    Transform[] transformParents;
+    Renderer[] bodyPartRenderers;
+    MaterialPropertyBlock materialPropertyBlock;  
 
     [Header("Explosion")]
     [SerializeField]
@@ -21,12 +26,51 @@ public class BodyExplosion : MonoBehaviour
     [SerializeField]
     bool isDead;
 
+    [SerializeField]
+    float dieCounter;
+    float currentDieCounter; 
+
     void Start()
     {
         rigidbodies = new Rigidbody[bodyParts.Length];
+        bodyPartColliders = new BoxCollider[bodyParts.Length];
+        bodyPartRenderers = new Renderer[bodyParts.Length];
+        iniPositions = new Vector3[bodyParts.Length];
+        transformParents = new Transform[bodyParts.Length];
+ 
         for (int i = 0; i < bodyParts.Length; i++)
         {
             rigidbodies[i] = bodyParts[i].GetComponent<Rigidbody>();
+
+            bodyPartColliders[i] = bodyParts[i].GetComponent<BoxCollider>();
+            bodyPartColliders[i].enabled = false;
+
+            bodyPartRenderers[i] = bodyParts[i].GetComponent<Renderer>();
+
+            iniPositions[i] = bodyParts[i].transform.localPosition;
+
+            transformParents[i] = bodyParts[i].transform.parent; 
+        }
+
+        materialPropertyBlock = new MaterialPropertyBlock(); 
+
+        currentDieCounter = dieCounter; 
+    }
+
+    void IgnoreCollisions()
+    {
+        for (int i = 0; i < bodyPartColliders.Length; i++)
+        {
+            Physics.IgnoreCollision()
+        }
+    }
+
+    public void PooledStart()
+    {
+        for (int i = 0; i < bodyParts.Length; i++)
+        {
+            bodyParts[i].transform.parent = transformParents[i];
+            bodyParts[i].transform.localPosition = iniPositions[i]; 
         }
     }
 
@@ -35,7 +79,35 @@ public class BodyExplosion : MonoBehaviour
         if (isDead)
         {
             Die();
-            isDead = false;
+            currentDieCounter -= Time.deltaTime;
+
+            for (int i = 0; i < bodyPartRenderers.Length; i++)
+            {
+                bodyPartRenderers[i].GetPropertyBlock(materialPropertyBlock);
+
+                Color newColor = new Color(255, 255, 255, currentDieCounter/255);
+                materialPropertyBlock.SetColor("_Color", newColor);
+
+                bodyPartRenderers[i].SetPropertyBlock(materialPropertyBlock); 
+            }
+
+            if (currentDieCounter <= dieCounter - 1)
+            {
+                for (int i = 0; i < bodyPartColliders.Length; i++)
+                {
+                    bodyPartColliders[i].enabled = true; 
+                }
+            }
+
+            if (currentDieCounter <= 0)
+            {
+                for (int i = 0; i < bodyParts.Length; i++)
+                {
+                    bodyParts[i].SetActive(false); 
+                }
+                isDead = false;
+                currentDieCounter = dieCounter;
+            }
         }
     }
 
@@ -52,6 +124,6 @@ public class BodyExplosion : MonoBehaviour
 
     float RandomExplosionForce()
     {
-        return Random.Range(explosionForce, explosionForce + 20);
+        return Random.Range(explosionForce, explosionForce + 1);
     }
 }
