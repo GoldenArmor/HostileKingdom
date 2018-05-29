f6e825f7195985faabda2726cd050aeca34ff66a
Quooler <marc.vallverdu.98@gmail.com>
2018-05-29 16:27:28 +0200

    Readded the body explosion to the project

---
+++ Assets/GameCharacters/Enemies/Scripts/BodyExplosion.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class BodyExplosion : MonoBehaviour
+{
+    [Header("Body Parts")]
+    [SerializeField]
+    GameObject fullBody;
+    [SerializeField]
+    GameObject[] bodyParts;
+    Rigidbody[] rigidbodies;
+
+    [Header("Explosion")]
+    [SerializeField]
+    Transform explosionCenter;
+    [SerializeField]
+    float explosionForce;
+    [SerializeField]
+    float upForce;
+    [SerializeField]
+    float explosionRadius;
+    [SerializeField]
+    bool isDead;
+
+    void Start()
+    {
+        rigidbodies = new Rigidbody[bodyParts.Length];
+        for (int i = 0; i < bodyParts.Length; i++)
+        {
+            rigidbodies[i] = bodyParts[i].GetComponent<Rigidbody>();
+        }
+    }
+
+    void Update()
+    {
+        if (isDead)
+        {
+            Die();
+            isDead = false;
+        }
+    }
+
+    public void Die()
+    {
+        for (int i = 0; i < rigidbodies.Length; i++)
+        {
+            bodyParts[i].transform.SetParent(null);
+            fullBody.SetActive(false);
+            bodyParts[i].SetActive(true);
+            rigidbodies[i].AddExplosionForce(RandomExplosionForce(), explosionCenter.position, explosionRadius, upForce, ForceMode.Impulse);
+        }
+    }
+
+    float RandomExplosionForce()
+    {
+        return Random.Range(explosionForce, explosionForce + 20);
+    }
+}
